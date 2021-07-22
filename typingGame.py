efa68f935e71431e3a47ae2389bd2c45dea3c785
thats a secret <jlhofstra@yahoo.com>
2021-07-22 22:31:09 +0900

    i put the basics together. I had to jog through some of the moves i made with the JS version. I was tempted to look back it to recover the logic, but tomorrow is a holiday, so i spaced out and gave myself a chance to remember. it took me about 10 minutes to put the pieces inplace, but i think it took a lot longer than that the first time i put it together in JS and a lot more little tests. it proves that real progress exists.

--- typingGame.py
+++ typingGame.py
@@ -1,13 +1,15 @@
 import random
 
-mylist = ["あ", "い", "う","え", "お","か", "き","く", "け","こ", "さ"]
+myKanjiList = ["雨", "自ら", "山","森", "川","海", "木","水", "上","下", "田"]
+myYomiList = ["あめ","みずら","やま","もり","かわ","うみ","き","みず","うえ","した","だ"]
 def function():
-    x = mylist[random.randint(0, 10)] 
+    randomNum = random.randint(0, 10)
+    x = myKanjiList[randomNum] 
     print("The letter is % s" % (x))
     def myinnerfunc():
         y = input("type the letter above:")
         def myinnerinnerfunc():
-          if y == x:
+          if y == myYomiList[randomNum]:
               print("ok")
           else:
               print("you made a typing mistake")
