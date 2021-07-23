ceaacf630c5fecee98100ef038c23f71cf76bb28
thats a secret <jlhofstra@yahoo.com>
2021-07-23 19:02:37 +0900

    added an input field and connected it with a button so the input prints when the button is clicked

--- gui.py
+++ gui.py
@@ -1,13 +1,15 @@
 from tkinter import *
 
 root = Tk()
+e = Entry(root, width = 40)
+e.pack()
 
 def myClick():
-    myLabel = Label(root, text="Look! I clciked a button!")
+    myLabel = Label(root, text=e.get())
     myLabel.pack()
 
 
-myButton = Button(root, text="click me!", pady=50, command=myClick)
+myButton = Button(root, text="Enter your name", pady=50, command=myClick, fg="blue", bg="red")
 myButton.pack()
 
 root.mainloop()
