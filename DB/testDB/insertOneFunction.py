140782105b4d69d4ddf601e3c0feb97f776b5baf
thats a secret <jlhofstra@yahoo.com>
2021-07-29 23:23:39 +0900

    やばい！I think i basically figured it out!.Figured out the basics at least!

--- DB/testDB/insertOneFunction.py
+++ DB/testDB/insertOneFunction.py
@@ -7,7 +7,6 @@ mycol = mydb["KanjiTest"]
 #function variables
 myKanjiList = ["雨", "自ら", "山","森", "川","海", "木","水", "上","下", "田"]
 myYomiList = ["あめ","みずら","やま","もり","かわ","うみ","き","みず","うえ","した","だ"]
-
 def function():
     randomNum = random.randint(0, 10)
     x = myKanjiList[randomNum] 
@@ -16,15 +15,18 @@ def function():
         y = input("type the letter above:")
         def myinnerinnerfunc():
           if y == myYomiList[randomNum]:
+              def yes():
+                mydict = { "count": 1, "question": [randomNum], "answer": 1 }
+                x = mycol.insert_one(mydict)
+              yes() 
               print("ok")
           else:
+              def no():
+                mydict = { "count": 1, "question": [randomNum], "answer": 0 }
+                x = mycol.insert_one(mydict)
+              no()
               print("you made a typing mistake")
         myinnerinnerfunc()
     myinnerfunc() 
 function()
 
-mydict = { "count": "1", "question": [x], "answer": [] }
-
-x = mycol.insert_one(mydict)
-
-print(x.inserted_id)
