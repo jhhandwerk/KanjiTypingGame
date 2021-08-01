024c337537bc1cbd43878dc4f3738cea4164fa7c
thats a secret <jlhofstra@yahoo.com>
2021-08-01 23:03:06 +0900

    updating the main file with all the changes i made in the test file. I also added a timestamp to be put in the DB with each round of the game

--- typingGame.py
+++ typingGame.py
@@ -1,5 +1,12 @@
 import random
+import pymongo
+import time
 
+#DB variables
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = myclient['testDB']
+mycol = mydb["KanjiTest"]
+#function variables
 myKanjiList = ["雨", "自ら", "山","森", "川","海", "木","水", "上","下", "田"]
 myYomiList = ["あめ","みずら","やま","もり","かわ","うみ","き","みず","うえ","した","だ"]
 def function():
@@ -10,8 +17,16 @@ def function():
         y = input("type the letter above:")
         def myinnerinnerfunc():
           if y == myYomiList[randomNum]:
+              def yes():
+                mydict = { "count": 1, "question": [randomNum], "answer": 1, "time": time.time() }
+                x = mycol.insert_one(mydict)
+              yes() 
               print("ok")
           else:
+              def no():
+                mydict = { "count": 1, "question": [randomNum], "answer": 0 }
+                x = mycol.insert_one(mydict)
+              no()
               print("you made a typing mistake")
         myinnerinnerfunc()
     myinnerfunc() 
